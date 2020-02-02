ba23fed801ebefbaed4380a4b3024d1441e1156f
GeorgiaAngeli <46467829+GeorgiaAngeli@users.noreply.github.com>
2020-02-01 21:53:55 -0500

    title scene
    and audio

---
+++ Reflectcelfer/Assets/TitleScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
+
+public class TitleScript : MonoBehaviour
+{
+    public GameObject Panel;
+
+    private void Start()
+    {
+        Panel.SetActive(false);
+    }
+
+    public void StartGame(int levelNum)
+    {
+        SceneManager.LoadSceneAsync(levelNum);
+    }
+
+    public void ShowCredits()
+    {
+        Panel.SetActive(true);
+    }
+
+    public void HideCredits()
+    {
+        Panel.SetActive(false);
+    }
+
+}
